ef0741acd355d69cc656842d1a0c66910c140596
d.soloviov <soloviovqqq@gmail.com>
2021-08-16 09:40:40 +0300

    footer fix

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -2,53 +2,42 @@
     <div class="container">
         <div class="footer__top">
             <div class="row">
-                <div class="col-lg-2">
+                <div class="col-lg-3">
                     <aside class="footer__top__nav">
-                        <h3>Company</h3>
+                        <h3><a href="{{ route('home') }}">Главная</a></h3>
                         <ul>
-                            <li><a title="About Us" href="#">About Us</a></li>
-                            <li><a title="Blog" href="#">Blog</a></li>
-                            <li><a title="Faqs" href="#">Faqs</a></li>
-                            <li><a title="Contact" href="#">Contact</a></li>
-                        </ul>
-                    </aside>
-                </div>
-                <div class="col-lg-2">
-                    <aside class="footer__top__nav">
-                        <h3>Company</h3>
-                        <ul>
-                            <li><a title="About Us" href="#">About Us</a></li>
-                            <li><a title="Blog" href="#">Blog</a></li>
-                            <li><a title="Faqs" href="#">Faqs</a></li>
-                            <li><a title="Contact" href="#">Contact</a></li>
-                        </ul>
-                    </aside>
-                </div>
-                <div class="col-lg-2">
-                    <aside class="footer__top__nav">
-                        <h3>Company</h3>
-                        <ul>
-                            <li><a title="About Us" href="#">About Us</a></li>
-                            <li><a title="Blog" href="#">Blog</a></li>
-                            <li><a title="Faqs" href="#">Faqs</a></li>
-                            <li><a title="Contact" href="#">Contact</a></li>
+                            <li><a title="About Us" href="#">Соединенные Штаты</a></li>
+                            <li><a title="Blog" href="#">Испания</a></li>
+                            <li><a title="Faqs" href="#">Нидерланды</a></li>
+                            <li><a title="Contact" href="#">Япония</a></li>
                         </ul>
                     </aside>
                 </div>
                 <div class="col-lg-3">
                     <aside class="footer__top__nav">
-                        <h3>Support</h3>
+                        <h3> Операторские туры</h3>
                         <ul>
-                            <li><a title="Get in Touch" href="#">Get in Touch</a></li>
-                            <li><a title="Help Center" href="#">Help Center</a></li>
-                            <li><a title="Live chat" href="#">Live chat</a></li>
-                            <li><a title="How it works" href="#">How it works</a></li>
+                            <li><a title="About Us" href="#">Гастро тур "Мамамия"</a></li>
+                            <li><a title="Blog" href="#">Пляжный тур "Ибица"</a></li>
+                            <li><a title="Faqs" href="#">Релакс тур "Насолода"</a></li>
+                            <li><a title="Contact" href="#">Гастро тур "Мамамия"</a></li>
+                        </ul>
+                    </aside>
+                </div>
+                <div class="col-lg-3">
+                    <aside class="footer__top__nav">
+                        <h3>Рекламные туры</h3>
+                        <ul>
+                            <li><a title="About Us" href="#">Гастро тур "Мамамия"</a></li>
+                            <li><a title="Blog" href="#">Пляжный тур "Ибица"</a></li>
+                            <li><a title="Faqs" href="#">Релакс тур "Насолода"</a></li>
+                            <li><a title="Contact" href="#">Гастро тур "Мамамия"</a></li>
                         </ul>
                     </aside>
                 </div>
                 <div class="col-lg-3">
                     <aside class="footer__top__nav footer__top__nav--contact">
-                        <h3>Контакты</h3>
+                        <h3><a href="{{ route('contacts') }}">Контакты</a></h3>
                         <p>г. Киев, ул. Богдана Хмельницкого 3А, оф.203</p>
                         <p><a href="mailto:book@travelbridge.com.ua">book@travelbridge.com.ua</a></p>
                         <p>+38 (066) 367 93 18</p>
